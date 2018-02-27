f2ec1ecbe395a0de90ad37a213402cc7ab3d9ba3
Instrutor <felpmel@gmail.com>
2018-02-27 16:38:03 -0300

    Primeiro Comite

---
+++ client/templates/home.js
@@ -0,0 +1,35 @@
+Template.home.created = function () {
+    console.log('Created the home template');
+};
+Template.home.rendered = function () {
+    console.log('Rendered the home template');
+};
+Template.home.destroyed = function () {
+    console.log('Destroyed the home template');
+};
+
+
+//To display data in the home template, we will create a helper function //that will return a simplestring as follows
+
+Template.home.helpers({
+    exampleHelper: function () {
+        return new Spacebars.SafeString('This text came from a helper with some<strong > HTML</strong >.');
+    }
+});
+
+Template.home.helpers({
+    // other helpers ...
+    dataContextHelper: function () {
+        return {
+            someText: 'This text was set using a helper of the parent    template.',
+            someNested: {
+                text: 'That comes from "someNested.text"'
+            }
+        };
+    }
+});
+
+Template.home.rendered = function () {
+    console.log('Rendered the home template');
+    this.find('p').innerHTML = 'We just replaced that text!';
+};
